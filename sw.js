2711774cdb6710e6b9d135a513f4e31b33429dee
Thalia-Dev07 <thaliaoficial7@gmail.com>
2024-07-12 19:52:36 -0300

    atualizando cache

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
 let cacheName = 'cache-v1';
 
+let currentCacheVersion = 1;
+
 self.addEventListener('install', (e) => {
 
-  let cache = caches.open(cacheName).then((c) => {
+  let cache = caches.open('cache-v' + currentCacheVersion).then((c) => {
+    // Adcione novos arquivos atualizados ao cache
     c.addAll([
-      // nothing
+      // Lista de arquivos para cache
     ]);
   });
 
@@ -15,9 +18,16 @@ self.addEventListener('fetch', function (event) {
 
   event.respondWith(
 
-    caches.open(cacheName).then(function (cache) {
+
+    caches.open('cache-v' + currentCacheVersion).then(function (cache) {
       return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function (response) {
+        // Verifique se a resposta em cache existe e é da versão atual
+        if (response && response.headers.get('Cache-Version') === currentCacheVersion) {
+          return response; // Use a resposta em cache
+        }
+
+        // Se a resposta em cache estiver desatualizada ou não for encontrada, busque na rede
+        return fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
           return response;
         });
@@ -27,3 +37,5 @@ self.addEventListener('fetch', function (event) {
   );
 
 });
+
+currentCacheVersion++;
